e5bbef3a87c8dbe89b09113adb247b695d56a8c8
Victor Lopez <vicrenlopez14115@gmail.com>
2021-11-29 21:29:07 -0600

    Creando otros objetos para nuestra escuela

---
+++ Entidades/Curso.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CorEscuela.Entidades
+{
+    public class Curso
+    {
+        public string UniqueId { get; private set; }
+
+        public string Nombre { get; set; }
+
+        public string Jornada { get; set; }
+
+        public Curso() => UniqueId = Guid.NewGuid().ToString();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,27 @@ namespace CorEscuela
             escuela.Pais = "Colombia";
             escuela.Ciudad = "Bogotá";
 
+            var curso1 = new Curso()
+            {
+                Nombre = "101"
+            };
+
+            var curso2 = new Curso()
+            {
+                Nombre = "201"
+            };
+
+            var curso3 = new Curso()
+            {
+                Nombre = "301"
+            };
+
             Console.WriteLine(escuela);
+            System.Console.WriteLine("============");
+            Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
+            Console.WriteLine($"{curso2.Nombre}, {curso2.UniqueId}");
+            Console.WriteLine(curso3);
+
         }
     }
 }
